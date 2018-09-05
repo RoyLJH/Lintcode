40fe14f92b252e23fcd9359f0a4370db4cde40d9
JiaHao Lu <38347064+RoyLJH@users.noreply.github.com>
2018-09-05 18:47:37 +0800

    Easy , 1 try

--- 0002-Trailing Zeros.cpp
+++ 0002-Trailing Zeros.cpp
@@ -6,12 +6,12 @@ public:
      */
     long long trailingZeros(long long n) {
         // write your code here, try to do it without arithmetic operators.
-        long long res = 0;
+        long long result = 0;
         while(n>0){
             res += n/5;
             n /= 5;
         }
-        return res;
+        return result;
     }
 };
  
